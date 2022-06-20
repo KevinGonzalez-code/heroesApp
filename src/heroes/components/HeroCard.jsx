51bf0cec6e30bed4199376614d225f3b799437f9
kevin <kevin.gonzalez-arnao@>
2022-06-20 16:46:33 +0200

    Fixed deploy the paths

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -11,7 +11,7 @@ export const HeroCard = ({
 }) => {
 
 
-    const heroUrl = `/assets/heroes/${id}.jpg`;
+    const heroUrl = `./assets/heroes/${id}.jpg`;
 
     return (
         <div className="col animate__animated animate__fadeIn">
